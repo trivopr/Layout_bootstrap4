13bdf7b27dac24f2ebe9776d2e6ecb144680e18c
Minhtri <minhtripr1605@gmail.com>
2017-08-27 17:58:12 +0700

    Hello

---
+++ New-layout2/app/assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+        $('section[data-type="background"]').each(function(){
+            var $this=$(this);
+            var $window=$(window);
+            $window.scroll(function(){
+                var y=-($window.scrollTop()/($this.data('speed')/2));
+                var position="100% "+y+"px";
+                $this.css({backgroundPosition:position});
+                
+            });
+        });
+        $(".image-member").hover(function(){
+                $(this).children( ".personal" ).slideDown(500);                
+            }, function(){
+                $(this).children( ".personal" ).slideUp(500);
+        });
+        
+       
+        
+});
